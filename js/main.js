007eb1c905378513c7f4cd8588113100b2660723
LanaRyelin <lanaryelin@gmail.com>
2021-02-26 12:51:58 +0500

    Создано модальное окно и подключен файл reset.scss

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
     $('.menu__burger').click(function() {
         $('.menu__burger, .header__menu, .menu').toggleClass('active');
     });
+
+    // Модальное окно
+    $('.btn-order').click(function() {
+        $('#modal').fadeIn(500);
+    });
+    $('.modal__close').click(function() {
+        $('#modal').fadeOut(500);
+    });
+    $('.modal__area').click(function() {
+        $('#modal').fadeOut(500);
+    });
 });
