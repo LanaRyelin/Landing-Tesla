d98f284fcc2a2ab9d9cf5d42fab5301f957f7ef7
LanaRyelin <lanaryelin@gmail.com>
2021-02-26 15:16:55 +0500

    Добавлены меняющийся title в header и favicon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Прокрутка на 2 секцию
     $('a[href^="#"]').click(function() {
         var target = $(this).attr('href');
         $('html, body').animate({
@@ -6,10 +7,23 @@ $(document).ready(function() {
         }, 500);
     });
 
+    // Выпадающее меню
     $('.menu__burger').click(function() {
         $('.menu__burger, .header__menu, .menu').toggleClass('active');
     });
 
+    // Изменяющийся title в header
+    $('.menu-list__item').click(function() {
+        let change = $(this).text();
+        let title = $('#header-title');
+        if(change != title.text()) {
+            $('.menu-list__item').removeClass('active');
+            $(this).addClass('active');
+            title.fadeOut(300, function() {
+                title.text(change).fadeTo( 600, 1, "linear" );
+            });
+        }        
+    });
     // Модальное окно
     $('.btn-order').click(function() {
         $('#modal').fadeIn(500);
